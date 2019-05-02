8278acef84563ca339697e2a491c4d49b3d8b19f
cchunzhe <cchunzhe@users.noreply.github.com>
2019-05-02 17:25:35 +0800

    added some nonsensical words

--- cz.cs
+++ cz.cs
@@ -1,2 +1,10 @@
 but wht if i just make a file that is not what the file says it is
 like a c# file without c# code haha
+
+
+if this then that, how is that this if that is that and this is this?
+im confusing myself
+
+using this and this using that.
+
+else why we live, never will i understand.
